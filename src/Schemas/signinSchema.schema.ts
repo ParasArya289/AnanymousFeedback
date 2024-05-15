44444b8246f4e7146c7ede1cc622733258d9f170
Paras Arya <parasarya289@gmail.com>
2024-05-15 11:49:44 +0530

    feat: more robust identifier shcema

--- src/Schemas/signinSchema.schema.ts
+++ src/Schemas/signinSchema.schema.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
+import { usernameValidation } from "./signupSchema.schema";
+
+const UsernameOrEmailSchema = z.union([
+  z.string().email("Invalid Email"), // Email validation
+  z.string().min(2, "Username must be atleast 2 character"), // Assuming a minimum length for usernames
+]);
 
 export const signInSchema = z.object({
-  identifier: z.string(),
+  identifier: UsernameOrEmailSchema,
   password: z.string(),
 });
