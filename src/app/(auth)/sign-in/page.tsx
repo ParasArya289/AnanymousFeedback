d1f642e69ebfb7ac552b1fad9c1799df0f89c723
Paras Arya <parasarya289@gmail.com>
2024-05-15 11:51:50 +0530

    feat: signIn page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,135 @@
 "use client";
-import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { signInSchema } from "@/Schemas/signinSchema.schema";
+import { signIn } from "next-auth/react";
 
-export default function Component() {
-  const { data: session, status } = useSession();
+const page = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
 
-  if (status === "authenticated") {
-    return <p>Signed in as {session.user.email}</p>;
-  }
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    // defaultValues: {
+    //   identifier: "",
+    //   password: "",
+    // },
+  });
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    setIsSubmitting(false);
+    if (result?.error) {
+      console.log(result)
+      if (result.error === "credentialsSignIn") {
+        toast("Login failed", {
+          description: "Incorrect username or password",
+        });
+      } else {
+        toast("Error", {
+          description: result?.error,
+        });
+      }
+    }
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+  };
 
-  return <a href="/api/auth/signin">Sign in</a>;
-}
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-background">
+      <div className="w-full max-w-md p-8 space-y-8 bg-background border rounded-lg shadow-sm">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-4xl mb-6">
+            Sign in
+          </h1>
+          <p className="mb-4">Sign in to start your ananymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email or Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Email/Username"
+                      type="text"
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Password"
+                      type="password"
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                </>
+              ) : (
+                "Signin"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member?
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              {" "}
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
