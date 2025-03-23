5ac88eceb443d33eb34f1ab92e37023f7ca6540b
ParasArya289 <parasarya289@gmail.com>
2025-03-24 00:14:02 +0530

    style: style changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { User } from "next-auth";
 import { signOut, useSession } from "next-auth/react";
 import React from "react";
@@ -10,14 +10,14 @@ const Navbar = () => {
   const user: User = session?.user as User;
 
   return (
-    <nav className="p-4 md:p-6 shadow-sm border-b">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+    <nav className="p-4 md:p-6 shadow-sm border-b ">
+      <div className="container mx-auto max-w-6xl flex flex-col md:flex-row justify-between items-center">
+        <a href="#" className="text-md font-bold mb-4 md:mb-0">
           Anonymous Feedback
         </a>
         {session ? (
           <>
-            <span className="mr-4">Welcome,{user.username || user.email}</span>
+            {/* <span className="mr-4">Welcome,{user.username || user.email}</span> */}
             <Button onClick={() => signOut()} className="w-full md:w-auto">
               Sign out
             </Button>
